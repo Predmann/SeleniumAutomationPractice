00373b5b1b762961df434ddd6f75bc11dec81365
Rudi <42889900+Predmann@users.noreply.github.com>
2022-11-09 13:49:19 +0100

    Add files via upload
    init comit

---
+++ src/main/java/automationpractice/com/pageObject/BrowserTest.java
@@ -0,0 +1,42 @@
+package automationpractice.com.pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTest {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+
+		// System.setProperty("webdriver.gecko.driver",
+		// projectPath+"/drivers/geckodriver/geckodriver.exe");
+		
+System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
+		// WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to wanted page
+		driver.get("http://automationpractice.com/index.php");
+
+		// Wait a little bit. I know that is not correct but I needed it for now.
+		// Will be removed soon due to using implicityWaits
+		Thread.sleep(3000);
+
+		// Find textBox and searchButton
+		WebElement textBox = driver.findElement(By.id("search_query_top"));
+		WebElement searchButton = driver.findElement(By.name("submit_search"));
+
+		// Type the text on the textBox and click on the searchButton
+		textBox.sendKeys("faded");
+		searchButton.click();
+
+		// Little pause
+		Thread.sleep(3000);
+		System.out.println("done");
+
+	}
+}
---
+++ src/main/java/automationpractice/com/pageObject/package-info.java
@@ -0,0 +1,1 @@
+package automationpractice.com.pageObject;
