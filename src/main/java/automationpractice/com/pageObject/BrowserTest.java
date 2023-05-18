6fc62297fd2a068e7c1a5f436fc6d19cf53b1e98
Przemek <przemyslaw.redmann@gmail.com>
2023-05-18 22:54:08 +0200

    Rebased to Java 17. Refactored BrowserTest.java

--- src/main/java/automationpractice/com/pageObject/BrowserTest.java
+++ src/main/java/automationpractice/com/pageObject/BrowserTest.java
@@ -4,39 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserTest {
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-		String projectPath = System.getProperty("user.dir");
-		System.out.println(projectPath);
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath);
+        System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
 
-		// System.setProperty("webdriver.gecko.driver",
-		// projectPath+"/drivers/geckodriver/geckodriver.exe");
+        if (driver.equals("new ChromeDriver()")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
 
-		System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
-		// WebDriver driver = new FirefoxDriver();
-		WebDriver driver = new ChromeDriver();
+        }
 
-		// Navigate to wanted page
-		driver.get("http://automationpractice.com/index.php");
+        // Navigate to wanted page
+        try {
+            driver.get("https://www.saucedemo.com/");
+        } catch (Exception e) {
+            throw new NullPointerException();
+        }
 
-		// Wait a little bit. I know that is not correct but I needed it for now.
-		// Will be removed soon due to using implicityWaits
-		Thread.sleep(3000);
+        // Wait a little bit. I know that is not correct but I needed it for now.
+        // Will be removed soon due to using implicityWaits
+        Thread.sleep(2000);
 
-		// Find textBox and searchButton
-		WebElement textBox = driver.findElement(By.id("search_query_top"));
-		WebElement searchButton = driver.findElement(By.name("submit_search"));
+        // Find textBox and searchButton
+        try{
+            WebElement loginButton = driver.findElement(By.id("login-button"));
+            System.out.println("all fine!");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-		// Type the text on the textBox and click on the searchButton
-		textBox.sendKeys("faded");
-		searchButton.click();
+       Thread.sleep(2000);
+        System.out.println("finish");
+        driver.quit();
+       //driver.close();
 
-		// Little pause
-		Thread.sleep(3000);
-		System.out.println("done");
-
-	}
+    }
 }
